2a38141cf1489a0193541e2d5ac7778756a299c7
mr-mixas <mixas.sr@gmail.com>
2016-05-22 01:12:46 +0300

    L_OVERRIDE added, HM_OVERRIDE renamed to R_OVERRIDE

--- lib/NDTools/HMBehs.pm
+++ lib/NDTools/HMBehs.pm
@@ -7,12 +7,31 @@ use parent 'Exporter';
 
 use Hash::Merge qw(_merge_hashes);
 
-our $VERSION = '0.01'; # Don't forget to change in pod below
+our $VERSION = '0.02'; # Don't forget to change in pod below
 our @EXPORT = qw(
-    HM_OVERRIDE
+    L_OVERRIDE
+    R_OVERRIDE
 );
 
-use constant HM_OVERRIDE => {
+use constant L_OVERRIDE => {
+    'SCALAR' => {
+        'SCALAR' => sub { $_[0] },
+        'ARRAY'  => sub { $_[0] },
+        'HASH'   => sub { $_[0] },
+    },
+    'ARRAY' => {
+        'SCALAR' => sub { [ @{ $_[0] } ] },
+        'ARRAY'  => sub { [ @{ $_[0] } ] },
+        'HASH'   => sub { [ @{ $_[0] } ] },
+    },
+    'HASH' => {
+        'SCALAR' => sub { $_[0] },
+        'ARRAY'  => sub { $_[0] },
+        'HASH'   => sub { _merge_hashes( $_[1], $_[0] ) },
+    },
+};
+
+use constant R_OVERRIDE => {
     'SCALAR' => {
         'SCALAR' => sub { $_[1] },
         'ARRAY'  => sub { $_[1] },
@@ -36,24 +55,38 @@ __END__
 
 =head1 NAME
 
-NDTools::HMBehs -- Collection of extra behaviours for HASH::Merge
+NDTools::HMBehs -- Collection of extra behaviors for HASH::Merge
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =head1 SYNOPSIS
 
     use Hash::Merge qw(merge);
-    use NDTools::HMBehs qw(HM_OVERRIDE);
+    use NDTools::HMBehs qw(R_OVERRIDE);
 
-    Hash::Merge::specify_behavior(HM_OVERRIDE);
+    Hash::Merge::specify_behavior(R_OVERRIDE);
 
     $result = merge($ref1, $ref2);
 
 =head1 EXPORT
 
-Nothing expoert by default.
+Nothing exports by default.
+
+=head1 BEHAVIORS
+
+=over 4
+
+=item L_OVERRIDE, R_OVERRIDE
+
+Merge hashes, override arrays: left and right precedence
+
+=back
+
+=head1 SEE ALSO
+
+L<Hash::Merge>
 
 =head1 LICENSE AND COPYRIGHT
 
