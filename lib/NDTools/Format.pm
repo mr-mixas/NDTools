b831e498bc74d1d20b659bab56a8a9416c537b74
mr-mixas <mixas.sr@gmail.com>
2016-06-21 09:57:09 +0300

    allow_nonref for JSON disabled by default, refactoring

--- lib/NDTools/Format.pm
+++ lib/NDTools/Format.pm
@@ -9,14 +9,29 @@ use parent qw(Exporter);
 use JSON qw();
 use Log::Log4Cli;
 
-our @EXPORT_OK = qw(
-    dec_json
-    enc_json
+our %EXPORT_TAGS = (
+    'JSON' => [qw(
+        dec_json
+        enc_json
+    )],
+);
+$EXPORT_TAGS{all} = [ map { @{$_} } values %EXPORT_TAGS ];
+our @EXPORT_OK = (
+    @{$EXPORT_TAGS{all}}
+);
+
+our %FORMATS = (
+    JSON => {
+        allow_nonref => 0,
+        canonical => 1,
+        pretty => 1,
+        relaxed => 1,
+    },
 );
 
 sub dec_json($;@) {
     my ($text, %opts) = @_;
-    my $struct = eval { JSON::from_json($text, {relaxed => 1, %opts}) };
+    my $struct = eval { JSON::from_json($text, {%{$FORMATS{JSON}}, %opts}) };
     return $struct unless ($@);
     log_error { $@ };
     return undef;
@@ -24,7 +39,7 @@ sub dec_json($;@) {
 
 sub enc_json($;@) {
     my ($struct, %opts) = @_;
-    my $text = eval { JSON::to_json($struct, {pretty => 1, canonical => 1, %opts}) };
+    my $text = eval { JSON::to_json($struct, {%{$FORMATS{JSON}}, %opts}) };
     return $text unless ($@);
     log_error { $@ };
     return undef;
