79d95c1d2f2bd0165a6fdc9d7b9041ed55abb4f3
mr-mixas <mixas.sr@gmail.com>
2016-06-27 21:35:23 +0300

    common tests added

---
+++ test/_common.mk
@@ -0,0 +1,24 @@
+# common tests and opts for all utils
+
+VERB=-vvvv
+
+define VRFY_EXIT_1
+e=$$?; [ $$e -ne 1 ] && { echo "--- WRONG EXIT CODE. EXPECTED: 1, GOT: $$e --- "; exit 255; } || true
+endef
+
+t_common: t_barebin t_help t_pod t_verbose t_version
+
+t_barebin: always
+	$(BIN); $(VRFY_EXIT_1)
+
+t_help: always
+	$(BIN) --help > help.got 2>&1; $(VRFY_EXIT_1)
+
+t_pod:
+	podchecker ../../$(BIN)
+
+t_verbose: always
+	$(BIN) -vv -v2 --verbose --verbose 3 ../alpha.json >/dev/null 2>&1 #check aliases and args
+
+t_version: always
+	$(BIN) --version --ver | grep --quiet --perl-regexp '^\d+\.\d+' # also to check aliases
