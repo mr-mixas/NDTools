3c1991bc17593c223a7258e55d408a0fc3526264
mr-mixas <mixas.sr@gmail.com>
2016-06-29 10:10:26 +0300

    claen target moved to test/_common.mk

--- test/_common.mk
+++ test/_common.mk
@@ -6,6 +6,9 @@ define VRFY_EXIT_1
 e=$$?; [ $$e -ne 1 ] && { echo "--- WRONG EXIT CODE. EXPECTED: 1, GOT: $$e --- "; exit 255; } || true
 endef
 
+clean:
+	rm -f *.got
+
 t_common: t_barebin t_help t_pod t_verbose t_version
 
 t_barebin: always
