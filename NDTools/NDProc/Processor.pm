be912d2d1df57e01bf20707d16623e9b413fb57b
mr-mixas <mixas.sr@gmail.com>
2016-11-26 00:07:54 +0300

    get_mod_opts() method added, mods collector refactored

--- NDTools/NDProc/Processor.pm
+++ NDTools/NDProc/Processor.pm
@@ -7,18 +7,32 @@ use warnings FATAL => 'all';
 
 use NDTools::INC;
 use Log::Log4Cli;
-use Module::Find qw(usesub);
+use Module::Find qw(findsubmod);
+
+sub get_mod_opts {
+    my ($self, $mod) = @_;
+    $mod = $self->{MODS}->{$mod}->new(); # will parse rest of args
+    return $mod->{OPTS};
+}
 
 sub list_avail_modules {
     my $self = shift;
-    return map { [ (split('::', $_))[-1], $_->VERSION, $_->MODINFO ] } sort keys %{$self->{MODS}};
+    return map { [ $_, $self->{MODS}->{$_}->VERSION, $self->{MODS}->{$_}->MODINFO ] }
+        sort keys %{$self->{MODS}};
 }
 
 sub new {
     my $self = bless {}, shift;
     for my $path (@_) {
         log_trace { "Loading plugins from $path" };
-        map { $self->{MODS}->{$_} = 1 } usesub $path;
+        for my $m (findsubmod $path) {
+            $self->{MODS}->{(split('::', $m))[-1]} = $m;
+        }
+    }
+    for my $m (keys %{$self->{MODS}}) {
+        log_trace { "Initializing module $m ($self->{MODS}->{$m})" };
+        eval "require $self->{MODS}->{$m}";
+        die_fatal "Failed to initialize module '$m' ($@)", 1 if ($@);
     }
     return $self;
 }
