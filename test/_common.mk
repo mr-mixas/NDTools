77e846cd22d49ddbfb17d261b73d926bfd16d1fa
mr-mixas <mixas.sr@gmail.com>
2016-11-29 10:04:53 +0300

    basic tests for ndproc's 'Remove' module added

--- test/_common.mk
+++ test/_common.mk
@@ -6,6 +6,10 @@ define VRFY_EXIT_1
 e=$$?; [ $$e -ne 1 ] && { echo "--- WRONG EXIT CODE. EXPECTED: 1, GOT: $$e --- "; exit 255; } || true
 endef
 
+define VRFY_EXIT_4
+e=$$?; [ $$e -ne 4 ] && { echo "--- WRONG EXIT CODE. EXPECTED: 4, GOT: $$e --- "; exit 255; } || true
+endef
+
 define VRFY_EXIT_8
 e=$$?; [ $$e -ne 8 ] && { echo "--- WRONG EXIT CODE. EXPECTED: 8, GOT: $$e --- "; exit 255; } || true
 endef
