0902087b6dfd914e9b0475d6275a6621bcbe5867
mr-mixas <mixas.sr@gmail.com>
2016-12-10 14:44:19 +0300

    base class for all ND tools

---
+++ NDTools/NDTool.pm
@@ -0,0 +1,50 @@
+package NDTools::NDTool;
+
+use strict;
+use warnings FATAL => 'all';
+
+use NDTools::INC;
+use Getopt::Long qw(:config bundling);
+use Log::Log4Cli;
+use Pod::Usage;
+
+sub VERSION { "n/a" }
+
+sub arg_opts {
+    my $self = shift;
+    return (
+        'help|h' => sub { $self->usage; exit 1 },
+        'verbose|v:+' => \$Log::Log4Cli::LEVEL,
+        'version|V' => sub { print $self->VERSION . "\n"; exit 0; },
+    );
+}
+
+sub configure {
+    my $self = shift;
+}
+
+sub defaults {
+    return {};
+}
+
+sub new {
+    my $self = bless {}, shift;
+    $self->{OPTS} = $self->defaults();
+    unless (GetOptions ($self->arg_opts)) {
+        $self->usage;
+        die_fatal "Unsupported opts used", 1;
+    }
+    $self->configure();
+    return $self;
+}
+
+sub usage {
+    pod2usage(
+        -exitval => 'NOEXIT',
+        -output => \*STDERR,
+        -sections => 'SYNOPSIS|OPTIONS|EXAMPLES',
+        -verbose => 99
+    );
+}
+
+1; # End of NDTools::NDTool
