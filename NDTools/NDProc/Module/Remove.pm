a680e9b41dea26656476be41376e97582b97ce53
mr-mixas <mixas.sr@gmail.com>
2017-03-15 10:16:59 +0300

    --ignore opt added

--- NDTools/NDProc/Module/Remove.pm
+++ NDTools/NDProc/Module/Remove.pm
@@ -7,21 +7,31 @@ use parent "NDTools::NDProc::Module";
 use NDTools::INC;
 use Log::Log4Cli;
 use Struct::Path qw(spath);
-use Struct::Path::PerlStyle qw(ps_parse);
+use Struct::Path::PerlStyle qw(ps_parse ps_serialize);
 
 sub MODINFO { "Remove specified parts from structure" }
-sub VERSION { "0.04" }
+sub VERSION { "0.05" }
 
 sub arg_opts {
     my $self = shift;
     return (
         $self->SUPER::arg_opts(),
+        'ignore=s@' => \$self->{OPTS}->{ignore},
         'strict' => \$self->{OPTS}->{strict},
     )
 }
 
 sub process {
     my ($self, $data, $opts) = @_;
+
+    my @stash;
+    for my $path (@{$opts->{ignore}}) {
+        log_debug { "Preserving '$path'" };
+        my $spath = eval { ps_parse($path) };
+        die_fatal "Failed to parse path ($@)", 4 if ($@);
+        push @stash, spath($data, $spath, deref => 1, paths => 1);
+    }
+
     for my $path (@{$opts->{path}}) {
         log_info { "Removing path '$path'" };
         my $spath = eval { ps_parse($path) };
@@ -32,6 +42,11 @@ sub process {
         }
         eval { spath($data, $spath, delete => 1, strict => $opts->{strict}) };
         die_fatal "Failed to remove path ($@)", 4 if ($@);
+    }
+
+    for my $s (@stash) {
+        log_debug { "Restoring '" . ps_serialize($s->[0]) . "'" };
+        ${(spath($data, $s->[0], expand => 1))[0]} = $s->[1];
     }
 }
 
@@ -50,6 +65,10 @@ Remove - remove specified parts from structure
 =item B<--[no]blame>
 
 Blame calculaton toggle. Enabled by default.
+
+=item B<--ignore> E<lt>pathE<gt>
+
+Preserve specified substructure. May be used several times.
 
 =item B<--path> E<lt>pathE<gt>
 
