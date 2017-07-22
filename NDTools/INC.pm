b03c1f22fcdb8e57acae78d2119110139fe6324b
mr-mixas <mixas.sr@gmail.com>
2017-07-22 13:22:20 +0300

    redundant code removed

--- NDTools/INC.pm
+++ NDTools/INC.pm
@@ -1,10 +1,7 @@
 package NDTools::INC;
 
-use strict;
-use warnings FATAL => 'all';
-
 sub import {
-    unshift @INC, substr(__FILE__, 0, length(__FILE__) - 3);
+    unshift @INC, substr(__FILE__, 0, -3);
 }
 
 1;
