a3d746d2bbaac847235369907886c12d6a78a8a4
mr-mixas <mixas.sr@gmail.com>
2018-03-01 10:17:06 +0300

    fix failures with some Getopt::Long versions

--- lib/App/NDTools/NDProc.pm
+++ lib/App/NDTools/NDProc.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings FATAL => 'all';
 use parent 'App::NDTools::NDTool';
 
-use Getopt::Long qw(:config bundling pass_through);
+use Getopt::Long qw(GetOptionsFromArray :config bundling pass_through);
 use Log::Log4Cli;
 use Module::Find qw(findsubmod);
 use App::NDTools::Slurp qw(s_decode s_dump s_encode);
@@ -13,7 +13,7 @@ use Struct::Diff 0.94 qw(diff split_diff);
 use Struct::Path 0.80 qw(path);
 use Struct::Path::PerlStyle 0.80 qw(str2path);
 
-our $VERSION = '0.24';
+our $VERSION = '0.25';
 
 sub arg_opts {
     my $self = shift;
@@ -133,10 +133,8 @@ sub exec {
         'version|V' => sub { print $self->VERSION . "\n"; exit 0 },
     );
 
-    my $p = Getopt::Long::Parser->new();
-    $p->configure('nopass_through'); # just to be sure
-    unless ($p->getoptionsfromarray($self->{ARGV}, @rest_opts)) {
-
+    Getopt::Long::Configure('nopass_through');
+    unless (GetOptionsFromArray($self->{ARGV}, @rest_opts)) {
         $self->usage;
         die_fatal "Unsupported opts passed", 1;
     }
